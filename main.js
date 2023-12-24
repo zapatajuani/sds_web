6ca35d0f95add75e50445b9707256d36cc9a2c18
zapatajuani <juaniLMC@gmail.com>
2023-12-23 21:56:24 -0300

    tercer commit

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-const posteos = document.querySelectorAll(".posteo_texto");
+let graficoHumedad = document.getElementById("#grafico_humedad_1")
 
-console.log(posteos[0]);
+var chart = new Chart(graficoHumedad, {
+    type: "line",
+    data: {
+        labels:["12p.m", "13p.m", "14p.m", "15p.m", "16p.m", "17p.m", "18p.m"],
+        datasets:[
+            {
+                label: "Sensor Humedad 1",
+                data: [30.5,31,30.5,29.7,29.0,28.5,27.9],
+            }
+        ]
+    }
+})
