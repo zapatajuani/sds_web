836f6b2d5b5a22c5b49a3a40e5690bc8ca5c207c
zapatajuani <juanilmc@gmail.com>
2024-01-07 03:03:51 -0300

    cambios de San Nicolas 2023.01.07

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
-let graficoHumedad = document.getElementById("graficoHumedad1");
+let graficoHumedad = document.getElementById("grafico_temperatura_1");
+let spanData = document.getElementById("sensor_humedad_1");
+let rango = document.getElementById("rango");
+let textoRango = document.getElementById("texto_rango");
+
+rango.addEventListener("change", () => {
+    textoRango.textContent = rango.value
+    if (rango.value < 20) {
+        spanData.innerHTML = "humidity_low"
+    } else if ((rango.value < 30) && (rango.value >= 20)) {
+        spanData.innerHTML = "humidity_mid"
+    } else {
+        spanData.innerHTML = "humidity_high"
+    }
+});
 
 new Chart(graficoHumedad, {
     type: "line",
